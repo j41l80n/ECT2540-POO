c1bd67ee0260f1024b9400a727f0f7fb4b97efc5
Jailson Santos <jailson_donald_cobain@hotmail.com>
2018-08-15 16:00:23 -0300

    atualizando

--- 15082018.py
+++ 15082018.py
@@ -1,31 +1,36 @@
 class NumerosComplexos:
 	#construtor
-    def __init__(self, parte_real=0, parte_imaginaria=0):
-        #membros
-        self.__parte_real = parte_real
-        self.__parte_imaginaria = parte_imaginaria
+	def __init__(self, parte_real=0, parte_imaginaria=0):
+		#membros
+		self.__parte_real = parte_real
+		self.__parte_imaginaria = parte_imaginaria
         
+        
+	def get_parte_real(self):
+		return self.__parte_real
     
-    def get_parte_real(self):
-        return self.__parte_real
-    
-    def set_parte_real(self, parte_real):
-        if type(parte_real) == float:
-            self.__parte_real = parte_real
-        else:
-            print('erro')
+	def set_parte_real(self, parte_real):
+		#if type(parte_real) == float:
+		self.__parte_real = parte_real
+		#else:
+		#	print('erro')
 
-    def get_parte_imaginaria(self):
-        return self.__parte_imaginaria
+	def get_parte_imaginaria(self):
+		return self.__parte_imaginaria
     
-    def set_parte_imaginaria(self, parte_imaginaria):
-        self.__parte_imaginaria = parte_imaginaria
+	def set_parte_imaginaria(self, parte_imaginaria):
+		self.__parte_imaginaria = parte_imaginaria
         
-    def impime_numero_complexo(self, numero):
+	def imprime_numero_complexo(self, numero):	
 		if:
+			print(numero)
 		else:
-        
-    re = property(get_parte_real, set_parte_real)
-    im = property(get_parte_imaginaria, set_parte_imaginaria)
+			print(numero)
+    
+	re = property(get_parte_real, set_parte_real)
+	im = property(get_parte_imaginaria, set_parte_imaginaria)
 
-	
+numero = NumerosComplexos();
+#numero.re(1)
+#numero.im(2)
+numero.imprime_numero_complexo(3)
