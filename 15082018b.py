df4ba7362f9b8a1f059b3ea2517f565e7b9ee9af
Jailson Santos <jailson_donald_cobain@hotmail.com>
2018-08-15 16:31:28 -0300

    15082018b

---
+++ 15082018b.py
@@ -0,0 +1,8 @@
+class contaBancaria:
+# construtor
+	def __init__(self, parte_real=0, parte_imaginaria=0):
+		# membros
+		self.__numero = numero
+		self.__titular = titular
+		self.__saldo = saldo
+		self.__limite = limite
