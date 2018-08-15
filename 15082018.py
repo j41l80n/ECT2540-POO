0d54b48be37ac907df074b0c0aafa77a51f5c700
Jailson Santos <jailson_donald_cobain@hotmail.com>
2018-08-15 16:27:18 -0300

    definindo as funcoes

--- 15082018.py
+++ 15082018.py
@@ -1,36 +1,50 @@
 class NumerosComplexos:
-	#construtor
+	# construtor
 	def __init__(self, parte_real=0, parte_imaginaria=0):
-		#membros
+		# membros
 		self.__parte_real = parte_real
 		self.__parte_imaginaria = parte_imaginaria
-        
-        
+		
+	# parte real
 	def get_parte_real(self):
 		return self.__parte_real
     
 	def set_parte_real(self, parte_real):
-		#if type(parte_real) == float:
 		self.__parte_real = parte_real
-		#else:
-		#	print('erro')
 
+	# parte imaginaria
 	def get_parte_imaginaria(self):
 		return self.__parte_imaginaria
     
 	def set_parte_imaginaria(self, parte_imaginaria):
 		self.__parte_imaginaria = parte_imaginaria
-        
-	def imprime_numero_complexo(self, numero):	
-		if:
-			print(numero)
-		else:
-			print(numero)
     
+    # funcoes property
 	re = property(get_parte_real, set_parte_real)
 	im = property(get_parte_imaginaria, set_parte_imaginaria)
 
+	
+	def imprime_numero_complexo(self, numero):	
+		if numero > 0:
+			print('teste {} teste {}' .format(0, 1))
+		else:
+			print(numero)
+		
+	def zerar_numero_real(self, numero):	
+		self.__parte_real = numero
+		self.__parte_imaginaria = numero
+		
+	def calcula_modulo(self):
+		pass
+	
+	def calcula_conjulgado(self):
+		pass
+		
+	def soma_complexos(self):
+		pass
+		
+	def comapra_complexos(self):
+		pass
+			
 numero = NumerosComplexos();
-#numero.re(1)
-#numero.im(2)
 numero.imprime_numero_complexo(3)
