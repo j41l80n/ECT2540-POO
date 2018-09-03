1f4ad9373b48a82f40198d1a42a55f8fbc4d8329
Jailson Santos <jailson_donald_cobain@hotmail.com>
2018-09-03 17:45:58 -0300

    finalizando lista

--- aula03092018.py
+++ aula03092018.py
@@ -8,30 +8,57 @@ class Lista:
 		self.__inicializa()
 
 	def __inicializa(self):
-		dados = [None] * capacidade
+		self.__dados = [None] * self.__capacidade
 		
 	def adiciona(self, object):
 		if self.__capacidade == self.__quantidade:
 			print('teste')
 		else:
-			dados[self.__capacidade-self.__quantidade] += object
+			self.__dados[self.__quantidade] = object
 			self.__quantidade += 1
-		pass
-
-	def get_objeto(self):
-		return object
-		# pass
 
 	def eh_vazio(self):
-		pass
+		if self.__quantidade != 0:
+			return False
+		else:
+			return True
 
-	def remove(self):
-		pass
+	def get_objeto(self, posicao):
+		if self.eh_vazio():
+			return 'sem objetos'
+		else:
+			return self.__dados[posicao]
 
+	def remove(self, posicao):
+		for i in range(posicao, self.__quantidade-1):
+			self.__dados[i] = self.__dados[i+1]
+
+		self.__dados[self.__quantidade-1] = None
+		self.__quantidade -= 1
 
 	def imprime(self):
-		pass
+		# for dado in self.__dados:
+		for i in range(0, self.__quantidade):
+			print(self.__dados[i])
 
-lista = Lista()
-lista.adiciona(2)
-print('fim do codigo')
+	def tamanho(self):
+		i = 0
+		for dado in self.__dados:
+			i += 1
+		return i
+
+lista = Lista(3)
+
+lista.adiciona('ufrn')
+lista.adiciona('ect')
+lista.adiciona('POO')
+# print(lista.eh_vazio())
+
+# print(lista.get_objeto(0))
+
+lista.remove(1)
+# print(lista.get_objeto(0))
+# print(lista.get_objeto(1))
+
+lista.imprime()
+print(lista.tamanho())
